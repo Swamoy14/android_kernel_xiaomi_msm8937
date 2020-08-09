3e34e8796f3fac12ab072722966abf41f0b9f2d3
sairam1411 <majgaonkars3@gmail.com>
2020-08-09 14:59:27 +0530

    Revert "drm/msm: dsi-staging: hook up state notifier"
    This reverts commit 3f8d5d05cd7654777e5998996afe3685dc18bde4.

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -19,9 +19,6 @@
 #include <linux/of_gpio.h>
 #include <video/mipi_display.h>
 #include <linux/firmware.h>
-#ifdef CONFIG_STATE_NOTIFIER
-#include <linux/state_notifier.h>
-#endif
 
 #include "dsi_panel.h"
 #include "dsi_ctrl_hw.h"
@@ -467,10 +464,6 @@ error_disable_vregs:
 	(void)dsi_pwr_enable_regulator(&panel->power_info, false);
 
 exit:
-#ifdef CONFIG_STATE_NOTIFIER
-	state_resume();
-#endif
-
 	return rc;
 }
 
@@ -496,10 +489,6 @@ static int dsi_panel_power_off(struct dsi_panel *panel)
 	rc = dsi_pwr_enable_regulator(&panel->power_info, false);
 	if (rc)
 		pr_err("[%s] failed to enable vregs, rc=%d\n", panel->name, rc);
-
-#ifdef CONFIG_STATE_NOTIFIER
-	state_suspend();
-#endif
 
 	return rc;
 }
